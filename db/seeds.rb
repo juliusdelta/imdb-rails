fede9ab6191549c82553d1a131da7c67660d279f
JD Gonzales <jd_gonzales@icloud.com>
2017-07-01 01:33:59 -0500

    Added movie model, testing suit, updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rake::Task['db:reset'].invoke
+
+20.times do
+  Movie.create(
+    title: Faker::Lorem.words.join(' '),
+    rating: ['NC17', 'R', 'PG13', 'PG', 'G'].sample
+  )
+end
+
+puts 'Seeded 20 records into the database'
