3d1b384437e6c459ee50308f10f280fbe1d10f22
JD Gonzales <jd_gonzales@icloud.com>
2017-07-06 00:15:57 -0500

    Removed rake task from seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Rake::Task['db:reset'].invoke
+# Rake::Task['db:reset'].invoke
 
 20.times do
   Movie.create(
